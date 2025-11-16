a320d27038ee9205d546bfb9cfa7855bb89a5ebc
Carlos Calderon <charly87calderon@gmail.com>
2025-11-16 10:33:48 -0600

    Update test cases and user roles in UsuarioPrueba

--- src/UsuarioPrueba.java
+++ src/UsuarioPrueba.java
@@ -11,7 +11,7 @@ public class UsuarioPrueba {
         // Los usuarios sin permisos que intentarán hacer cambios
         Usuario maestroJohn = new Usuario(2, "John Maestro", "john@escuela.com", "contrama",
                 "Maestro");
-        Usuario estudianteSofia = new Usuario(3, "Sofia Estudiante", "sofia@escuela.com", "claveest1",
+        Usuario estudianteSofia = new Usuario(3, "Sofia Estudiante", "sofia@escuela.com", "clavealum",
                 "Estudiante");
 
         System.out.println("*** INICIO DE PRUEBAS DE LA CLASE USUARIO ***");
@@ -20,27 +20,27 @@ public class UsuarioPrueba {
 
         // *** Prueba 1: cambiar el rol *Éxito*
         // Verificamos si el Admin puede cambiar el rol de un Alumno a Maestro
-        System.out.println(" Prueba 1: el administrator cambia rol de Sofia (Éxito esperado):");
+        System.out.println(" Prueba 1: el administrator cambia rol de Sofia (Debería funcionar):");
         estudianteSofia.asignarRol("Maestro", adminPrincipal);
         System.out.println("Rol actual de Sofia: " + estudianteSofia.getTipoUsuario() + "\n");
 
         // *** Prueba 2: cambiar rol **Falla por Permisos** ***
         // Verificamos si un Maestro puede cambiar el rol de Sofia
-        System.out.println("-> Prueba 2: Maestro intenta cambiar el rol de Sofia (Falla esperada):");
+        System.out.println("-> Prueba 2: El maestro intentara cambiar el rol de Sofia (Debe denegar):");
         // John **Maestro** intenta cambiar el rol, pero no tiene el permiso de "Admin"
         estudianteSofia.asignarRol("Admin", maestroJohn);
         System.out.println("Rol actual de Sofia: " + estudianteSofia.getTipoUsuario() + "\n");
 
         // *** Prueba 3: Cambio de Contraseña **Éxito** ***
         // Verificamos si el Admin puede restablecer la contraseña de John
-        System.out.println("-> Prueba 3: administrator restablece clave de John (Éxito esperado):");
+        System.out.println("-> Prueba 3: administrator restablece clave de John (Debería funcionar):");
         maestroJohn.restablecerContrasena("Temporal2025*", adminPrincipal);
         System.out.println("Nueva clave para John (simulación de restablecimiento).\n");
 
 
 
         // *** Prueba 4: estado de la cuenta (Conexión con la Persona 4) ***
-        // Simulación: La lógica de mora (Persona 4) detecta que John tiene un libro vencido
+        // Simulación: la lógica de mora (Persona 4) detecta que John tiene un libro vencido
         System.out.println("\n-> Prueba 4: Simulación de bloqueo por Mora:");
         maestroJohn.setEstadoCuenta("Bloqueado");
         System.out.println("   Estado de la cuenta de John: " + maestroJohn.getEstadoCuenta());
